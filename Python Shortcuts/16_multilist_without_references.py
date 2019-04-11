a1d55683c02f294c406ffbdfbc2be589704afeb9
Néstor Gómez <ntr02@hotmail.com>
2019-04-10 23:46:58 -0500

    Update 16_multilist_without_references.py

--- Python Shortcuts/16_multilist_without_references.py
+++ Python Shortcuts/16_multilist_without_references.py
@@ -1,20 +1,20 @@
 # Create a list of lists using comprehensions 
 # so items are not immutable.
 def CreateMultilistNoReference():
-	multi = [[0 for col in range(5)] for row in range(10)]
-	multi[0][0] = 'Changed'
+    multi = [[0 for col in range(5)] for row in range(10)]
+    multi[0][0] = 'Changed'
 
-	for n in multi: print(n)
+    for n in multi: print(n)
 
 
 # Create a list of lists by multiplying  
 # so items are mutable.
 def CreateMultilistReference():
-	row = [0] * 5
-	multi = [row] * 10
-	multi[0][0] = 'Changed'
+    row = [0] * 5
+    multi = [row] * 10
+    multi[0][0] = 'Changed'
 
-	for n in multi: print(n)
+    for n in multi: print(n)
 
 
 CreateMultilistNoReference()
